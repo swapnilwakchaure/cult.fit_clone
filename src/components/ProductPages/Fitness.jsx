e1ba2b6d818411b0645d6315a8d2592601883ac5
Swapnil Wakchaure <swapnilwakchaure2411999@gmail.com>
2022-11-12 13:25:03 +0530

    day4 work

--- src/components/ProductPages/Fitness.jsx
+++ src/components/ProductPages/Fitness.jsx
@@ -1,7 +1,31 @@
+import { cult_fit_data } from "../cult_fit_data";
+import fitStyles from "./fitness.module.css";
+import Footer from "../Footer/Footer";
+
 const Fitness = () => {
+    // console.log(cult_fit_data);
     return (
         <div>
-            <p>use our app and make your Fitness</p>
+            <div className={fitStyles.heading}>
+                <h1>Lab Tests</h1>
+                <p>Assess and monitor your health from the comfort of your home now</p>
+            </div>
+            <div className={fitStyles.cult_fit_products}>
+                {cult_fit_data.map((item) => (
+                    <div key={item.id}>
+                        <img src={item.image} alt="cult-fit-image" />
+                        <div>
+                            <p>Offer Price</p>
+                            <p>₹{item.offer_price}</p>
+                        </div>
+                        <div>
+                            <p>Price</p>
+                            <p>₹{item.price}</p>
+                        </div>
+                    </div>
+                ))}
+            </div>
+            <Footer />
         </div>
     )
 }
